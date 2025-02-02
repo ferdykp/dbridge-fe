ac08e3cfad2d51eaa06a7508d5e523563d9c1f67
kimp <ahmadmiqdad27@gmail.com>
2025-02-02 13:52:24 +0700

    Add permission for users view

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -56,26 +56,28 @@
                     <span class="nav-link-text ms-1">Profile</span>
                 </a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link {{ request()->routeIs('users.create') ? 'bg-primary text-white' : '' }}"
-                    href="{{ route('users.create') }}">
-                    <div
-                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
-                    </div>
-                    <span class="nav-link-text ms-1">Create Users</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link {{ request()->routeIs('users.index', 1) ? 'bg-primary text-white' : '' }}"
-                    href="{{ route('users.index', 1) }}">
-                    <div
-                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
-                    </div>
-                    <span class="nav-link-text ms-1">Show Users</span>
-                </a>
-            </li>
+            @if (Auth::user()->role == 'sm')
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('users.create') ? 'bg-primary text-white' : '' }}"
+                        href="{{ route('users.create') }}">
+                        <div
+                            class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
+                        </div>
+                        <span class="nav-link-text ms-1">Create Users</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('users.index', 1) ? 'bg-primary text-white' : '' }}"
+                        href="{{ route('users.index', 1) }}">
+                        <div
+                            class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
+                        </div>
+                        <span class="nav-link-text ms-1">Show Users</span>
+                    </a>
+                </li>
+            @endif
         </ul>
     </div>
 </aside>
