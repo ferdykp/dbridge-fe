135d939d07bb9f58671cc2c9e69c1b1d51396bc4
kimp <ahmadmiqdad27@gmail.com>
2025-02-13 16:17:05 +0700

    Fix navbar in create

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -543,7 +543,7 @@
     </footer>
     </div>
     </main>
-    <div class="fixed-plugin">
+    {{-- <div class="fixed-plugin">
         <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
             <i class="fa fa-cog py-2"> </i>
         </a>
@@ -631,13 +631,13 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
     <!--   Core JS Files   -->
-    <script src="../assets/js/core/popper.min.js"></script>
+    {{-- <script src="../assets/js/core/popper.min.js"></script>
     <script src="../assets/js/core/bootstrap.min.js"></script>
     <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
     <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
-    <script src="../assets/js/plugins/chartjs.min.js"></script>
+    <script src="../assets/js/plugins/chartjs.min.js"></script> --}}
     <script>
         var ctx1 = document.getElementById("chart-line").getContext("2d");
 
@@ -731,9 +731,9 @@
         }
     </script>
     <!-- Github buttons -->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    {{-- <script async defer src="https://buttons.github.io/buttons.js"></script> --}}
     <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
-    <script src="../assets/js/argon-dashboard.min.js?v=2.1.0"></script>
+    {{-- <script src="../assets/js/argon-dashboard.min.js?v=2.1.0"></script> --}}
     {{-- Get StockCode Data --}}
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
