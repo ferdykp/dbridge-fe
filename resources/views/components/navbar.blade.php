24fe922853bd01439a137e0c4ba1ebf6bb76d4cb
ferdykp <kpferdy@gmail.com>
2025-02-13 14:31:59 +0700

    edit navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,15 +1,38 @@
 <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur"
     data-scroll="false">
     <div class="container-fluid py-1 px-3">
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                <li class="breadcrumb-item text-sm">
-                    <a class="opacity-5 text-white" href="javascript:;">Pages</a>
-                </li>
-                <li class="breadcrumb-item text-sm text-white active" aria-current="page">Dashboard</li>
-            </ol>
-            <h6 class="font-weight-bolder text-white mb-0">Dashboard</h6>
-        </nav>
+        @php
+            $segments = request()->segments();
+            $lastSegment = end($segments);
+            $breadcrumb = ucfirst(str_replace('-', ' ', $lastSegment));
+        @endphp
+
+        <div class="p-3 rounded-3" style="background-color: rgba(128, 0, 0, 0.8);">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+                    <li class="breadcrumb-item text-sm">
+                        <a class="opacity-75 text-white" href="{{ url('/') }}">Home</a>
+                    </li>
+                    @foreach ($segments as $index => $segment)
+                        @php
+                            $url = url(implode('/', array_slice($segments, 0, $index + 1)));
+                            $isLast = $loop->last;
+                        @endphp
+                        <li class="breadcrumb-item text-sm {{ $isLast ? 'text-white active' : '' }}"
+                            aria-current="{{ $isLast ? 'page' : '' }}">
+                            @if ($isLast)
+                                {{ ucfirst(str_replace('-', ' ', $segment)) }}
+                            @else
+                                <a class="opacity-75 text-white"
+                                    href="{{ $url }}">{{ ucfirst(str_replace('-', ' ', $segment)) }}</a>
+                            @endif
+                        </li>
+                    @endforeach
+                </ol>
+                <h6 class="font-weight-bolder text-white mb-0">{{ $breadcrumb }}</h6>
+            </nav>
+        </div>
+
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
             <div class="ms-md-auto pe-md-3 d-flex align-items-center">
                 {{-- <div class="input-group">
