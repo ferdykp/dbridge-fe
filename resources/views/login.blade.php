4dcfe19006f91b779ae83c4c8fd891df4d35e884
ferdykp <kpferdy@gmail.com>
2025-02-13 19:30:53 +0700

    font login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,10 +4,11 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/logo-mining-bg.jpg') }}">
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/logo-mining-bg.jpg') }}">
+    <link id="pagestyle" href="{{ asset('assets/css/argon-dashboard.css?v=2.1.0') }}" rel="stylesheet">
     <title>
-        Login D-Bridge
+        D-Bridge
     </title>
     <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
@@ -18,6 +19,19 @@
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     <!-- CSS Files -->
     <link id="pagestyle" href="../assets/css/argon-dashboard.css?v=2.1.0" rel="stylesheet" />
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <style>
+        .custom-dropdown-box {
+            background-color: #8e121f;
+            /* opacity: 0.9; */
+            border-radius: 20px;
+            padding: 10px;
+            z-index: 1;
+            /* Optional: To add some space inside the box */
+        }
+    </style>
 </head>
 
 <body class="">
@@ -34,7 +48,22 @@
                     <div class="row">
                         <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
                             <div class="card card-plain">
-                                <div class="card-header pb-0 text-start">
+                                <div class="card-header pb-0 text-start text-center">
+                                    <!-- Kotak teks -->
+                                    <div style="
+                                background: rgba(146, 6, 6, 0.7);
+                                padding: 10px 20px;
+                                border-radius: 6px;
+                                color: #fdfdfd;
+                                font-weight: bold;
+                                font-size: 30px;
+                                display: inline-block;
+                                margin-bottom: 70px;
+                                width:400px;
+                            ">
+                                        Here, There and Everywhere
+                                    </div>
+
                                     <h4 class="font-weight-bolder">Sign In</h4>
                                     <p class="mb-0">Enter your email and password to sign in</p>
                                 </div>
@@ -72,29 +101,22 @@
                                                 onclick="togglePassword()">
                                             <label for="showPassword">Show Password</label>
                                         </div>
-                                        {{-- <div class="form-check form-switch">
-                                            <input class="form-check-input" type="checkbox" id="rememberMe">
-                                            <label class="form-check-label" for="rememberMe">Remember me</label>
-                                        </div> --}}
+
                                         <div class="text-center">
                                             <button type="submit"
-                                                class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Sign in</button>
+                                                class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Sign
+                                                in</button>
                                         </div>
                                     </form>
                                 </div>
-                                {{-- <div class="card-footer text-center pt-0 px-lg-2 px-1">
-                  <p class="mb-4 text-sm mx-auto">
-                    Don't have an account?
-                    <a href="javascript:;" class="text-primary text-gradient font-weight-bold">Sign up</a>
-                  </p>
-                </div> --}}
                             </div>
                         </div>
+
+                        <!-- Bagian Background Gambar -->
                         <div
                             class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
                             <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden"
                                 style="background-image: url('/assets/img/mining.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat;">
-                                <span class="mask bg-gradient-primary opacity-0"></span>
                             </div>
                         </div>
                     </div>
